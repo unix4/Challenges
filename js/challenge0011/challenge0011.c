a7b8268105a5cb13ca2f86a12eef379a9600cfa3
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-07 21:27:56 +0200

    Update challenge0011.c

--- js/challenge0011/challenge0011.c
+++ js/challenge0011/challenge0011.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int circuihowManySecondstPower(const int hours) {
+int howManySeconds(const int hours) {
    return hours * 60 * 60;
 }
 
 int main() {
-    printf("%i\n", circuihowManySecondstPower(2));
-    printf("%i\n", circuihowManySecondstPower(10));
-    printf("%i\n", circuihowManySecondstPower(24));
+    printf("%i\n", howManySeconds(2));
+    printf("%i\n", howManySeconds(10));
+    printf("%i\n", howManySeconds(24));
    
     return 0;
 }
