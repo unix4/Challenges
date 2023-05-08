ff79453a102381230bb26664fac550e5c438af3e
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-08 16:06:15 +0200

    Update challenge0013.rkt

--- js/challenge0013/challenge0013.rkt
+++ js/challenge0013/challenge0013.rkt
@@ -1,8 +1,10 @@
 #lang racket
 
+; had to define my one method, since remainder already exists
 (define (my_remainder x y )
   (remainder x y))
   
+
 (remainder 1 3)
 (remainder 3 4)
 (remainder -9 45)
