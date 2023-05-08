a1309612db94938e7a5ab579e8417858bb5eef95
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-08 22:21:54 +0200

    Create challenge0015.pl

---
+++ js/challenge0015/challenge0015.pl
@@ -0,0 +1,9 @@
+sub giveMeSomething{
+	"something $_[0]";
+}
+
+print giveMeSomething("is better than nothing");
+print "\n";
+print giveMeSomething("Bob Jane");
+print "\n";
+print giveMeSomething("something");
