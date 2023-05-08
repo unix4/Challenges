b8534649090551a39cb0598f82c009ffcb6f2461
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-08 22:18:11 +0200

    Create challenge0015.js

---
+++ js/challenge0015/challenge0015.js
@@ -0,0 +1,5 @@
+const giveMeSomething = (str) => "something " + str;
+
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("Bob Jane"));
+console.log(giveMeSomething("something"));
