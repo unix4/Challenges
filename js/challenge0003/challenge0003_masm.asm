157661013564ec86931c9b60e044a2b07648b2b4
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-21 16:39:48 +0200

    Create challenge0003_masm.asm

---
+++ js/challenge0003/challenge0003_masm.asm
@@ -0,0 +1,28 @@
+; AddTwo.asm - adds two 32-bit integers.
+; Chapter 3 example
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+include c:\masm\Irvine\Irvine32.inc
+includelib c:\masm\Irvine\Irvine32.lib
+
+.code
+main proc		
+	mov esi, 2
+	call convert
+	call WriteInt
+
+	invoke ExitProcess,0
+main endp
+
+convert proc
+	mov eax, esi
+	mov ebx, 60
+	mul ebx
+	ret
+convert endp
+
+end main
