3badbcc93a18969ffcfff0f194e9786fca5e6266
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-29 20:50:59 +0200

    Create challenge0014_masm.asm

---
+++ js/challenge0014/challenge0014_masm.asm
@@ -0,0 +1,58 @@
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+include c:\masm\Irvine\Irvine32.inc
+includelib c:\masm\Irvine\Irvine32.lib
+
+Str_copy PROTO,
+	source: PTR BYTE,
+	target: PTR BYTE
+
+
+.data
+something BYTE "something ",0
+my_text1 BYTE "Bob Jane",0
+
+my_result BYTE 200 DUP(?),0
+
+
+.code
+main proc	
+	call giveMeSomething	
+	invoke ExitProcess,0
+main endp
+
+; Useful
+str_append proc
+	mov esi, 0
+	mov edi, 0
+str1_copy:
+	mov al, something[esi]
+	mov my_result[esi], al
+	inc esi
+	cmp something[esi], 0
+	jz str2_copy
+	jmp str1_copy
+str2_copy:
+	mov al, my_text1[edi]
+	mov my_result[esi], al
+	inc esi
+	inc edi
+	cmp my_text1[edi], 0
+	jz done
+	jmp str2_copy
+done:
+	ret
+str_append endp
+
+giveMeSomething proc 
+	call str_append
+	mov edx,OFFSET my_result
+	call WriteString
+
+	ret
+giveMeSomething endp
+
+end main
